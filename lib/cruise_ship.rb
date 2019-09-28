8fad73724f5e6668f791b72d533a8b73aa11f682
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-28 22:48:11 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,9 +1,9 @@
 def select_winner(passangers)
 passengers = {
-suite_a: "Amanda Presley",  
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
 winner = ""
